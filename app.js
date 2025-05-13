46afbbc50fb9987141fae9f5a6ea7bdfb98d6cc2
RuslanSkl <rsklyarenko@gmail.com>
2025-05-13 17:56:26 +0300

    - express.js adminData

--- app.js
+++ app.js
@@ -3,13 +3,14 @@ const bodyParser = require('body-parser');
 const adminRoutes = require('./routes/admin');
 const shopRoutes = require('./routes/shop');
 const path = require("path");
+const adminData = require("./routes/admin");
 
 const app = express();
 
 app.use(bodyParser.urlencoded({extended: false}));
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/admin', adminRoutes);
+app.use('/admin', adminData.routes);
 app.use(shopRoutes);
 
 app.use((req, res, next) => {
